d3393fbf50260d9c4ab4667174b969e295c48db2
Oriana Keserue <o.keseruepittet@gmail.com>
2026-05-02 10:53:28 +0200

    finished tutorial

--- first-website/scripts/main.js
+++ first-website/scripts/main.js
@@ -8,3 +8,26 @@ myImage.addEventListener("click", () => {
     myImage.setAttribute("src", "images/images.jpeg");
   }
 });
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name."); //uses what the user writes to store in myName variable
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName); //uses web storage API to store myName variable in the browser
+    myHeading.textContent = `Mozilla is cool, ${myName}`; //sets text content of the heading using user's input}
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  //initialization code; runs first when page loads. Here, if there's no name in localStorage, runs the above function
+} else {
+  const storedName = localStorage.getItem("name"); //otherwise, get back the name and change heading text content
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  //when button is clicked, setUserName() runs. Allows user to change name.
+  setUserName();
+});
